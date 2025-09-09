a5611c3f9da200831b4b63dd64a6f04c085b8d93
Vishal Tripathy <tripathyvishal7@gmail.com>
2025-09-09 22:31:05 +0530

    Initial Commit having First Project made using React and Tailwind

---
+++ Movie-Mania/src/App.jsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+import { useDebounce } from "react-use";
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [moviesList, setMoviesList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+
+  const fetchMovies = async (query) => {
+    setLoading(true);
+    setErrorMessage('');
+
+    try {
+      const response = await fetch(
+        query
+          ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+          : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`,
+        API_OPTIONS
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      if (data.response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch Data");
+        setMoviesList([]);
+        return;
+      }
+
+      setMoviesList(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies:, ${error}`);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  return (
+    <>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="hero banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll enoy
+            watching with <span className="text-gradient">PopCorn</span>
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {loading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {moviesList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default App;
